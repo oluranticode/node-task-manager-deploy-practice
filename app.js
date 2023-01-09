b290979906303890761a2e8051d169ac130e1bc7
temitope.aluko <temitope.aluko@flutterwavego.com>
2023-01-09 20:58:52 +0100

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
+// require the database
+const connectDB = require('./db/connect');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4700;
-// const connectDB = require('./db/connect');
+const tasks = require('./routes/task');
+const PageNotFound = require('./middleware/pageNotFound');
+const errorHandler = require('./middleware/error_handler');
 require('dotenv').config();
-const tasks = require('./routes/task-route');
-const notFound = require('./middleware/not-found');
+// const port = 5000;
+const port = process.env.PORT || 4700;
 
 
-
-
-
+// middlesware
 app.use(express.json());
-// app.use(notFound);
-// app.use(express.static('./public'));
+// app.use(PageNotFound);
+app.use(express.static('./public'));
+app.use(errorHandler)
 
+// route
+    app.use('/api/v1/tasks', tasks);
 
-
-app.get("/", (req, res)=>{
-    res.send("working for temzy!!!");
-});
-
-app.use('api/v1/tasks', tasks);
+//app.get('api/v1/tasks') -   get all the tasks
+//app.post('api/v1/tasks') -   create a new task
+//app.get('api/v1/tasks/:id') -   get a single tasks
+//app.patch('api/v1/tasks/:id') -   update task
+//app.delete('api/v1/tasks/:id') -   delete task
 
 const start = async () => {
     try {
-        // await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, ()=>{
-            console.log(`port listening to ${port}`);
+            console.log(`Server listening to port ${port}`)
         });
-        
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
-start();
 
+start();
--- error/custom-error.js
+++
@@ -1,6 +0,0 @@
-// trow an error
-const throwAnError = () => {
-    throw new Error('something went wrong');
-}
-
-module.exports = throwAnError
